1c031b3af4f0cea48589eb94062d85e318d49eb2
rieperrebeca <rsilva11@byupathway.edu>
2025-09-17 22:39:11 -0300

    update

--- week02/mealcalculation.py
+++ week02/mealcalculation.py
@@ -2,14 +2,19 @@
 
 child_meal = float(input("What is the price of a child's meal?"))
 adult_meal = float(input("What is the price of an adult's meal?"))
+drink_price = float(input("What is the price of drinks?"))
+Snack = float(input("What is the price of a snack?"))
+print("")
 children = int(input("How many children are there?"))
 adults = int(input("How many adults are there?"))
-drink_price = float(input("What is the price of drinks?"))
 drink = int(input("How many drinks were ordered?"))
+snack_price = int(input("How many snacks were ordered?"))
+
+
 
 #calculate the value
-subtotal = (child_meal * children) + (adult_meal * adults) + (drink_price * drink)
-tax = subtotal * 3.5
+subtotal = (child_meal * children) + (adult_meal * adults) + (drink_price * drink) + (Snack * snack_price)
+tax = subtotal * .5
 total_with_tax = subtotal + tax
 total_with_tax_and_tip = total_with_tax * 1.5
 
@@ -24,4 +29,5 @@ print(f"The total cost of the meal with tax is and tip is: ${total_with_tax_and_
 #change calculation
 payment = float(input("What is the payment amount?"))
 change = payment - total_with_tax
+print("")
 print(f"The change due is: ${change:.2f}")
